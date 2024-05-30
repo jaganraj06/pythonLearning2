1e0f2bd05c3b0c4b79e99cd39b3216a3eb30f5b0
jaganraj06 <167900515+jaganraj06@users.noreply.github.com>
2024-05-30 17:19:28 +0530

    j

---
+++ share.py
@@ -0,0 +1,24 @@
+import multiprocessing
+
+
+def calc_square(numbers,result,v):
+    v.value = 5.67
+    for idx, n in enumerate(numbers):
+        print(str(n))
+        print(str(idx))
+        result[idx] = n*n
+
+
+
+if __name__ == "__main__":
+    numbers =[2,3,4]
+    result = multiprocessing.Array('i',3)
+    v = multiprocessing.Value('d',0.0)
+    p = multiprocessing.Process(target=calc_square, args=(numbers,result,v))
+
+
+    p.start()
+    p.join()
+
+    print(v.value)
+    print(result[:])
